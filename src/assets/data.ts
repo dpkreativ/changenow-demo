2f6ba8273e9eb2c9d45947a752d5cb78c83dde48
Divine Orji <dpkreativ@gmail.com>
2023-12-19 14:45:06 +0100

    feature: add placeholder data

---
+++ src/assets/data.ts
@@ -0,0 +1,57 @@
+export const cryptoCoins = [
+  {
+    name: 'Bitcoin',
+    description:
+      'Bitcoin is the first decentralized digital currency. It is a peer-to-peer currency that operates without a central authority or banks.',
+    slug: 'bitcoin',
+    seoProperties: {
+      metaTitle: 'Bitcoin - The Original Cryptocurrency',
+      metaDescription:
+        'Learn about Bitcoin, the pioneering cryptocurrency that introduced blockchain technology to the world.',
+    },
+  },
+  {
+    name: 'Ethereum',
+    description:
+      'Ethereum is a decentralized platform that enables developers to build and deploy smart contracts and decentralized applications (dApps).',
+    slug: 'ethereum',
+    seoProperties: {
+      metaTitle: 'Ethereum - Decentralized Smart Contract Platform',
+      metaDescription:
+        'Explore Ethereum, a leading blockchain platform supporting smart contracts and decentralized applications.',
+    },
+  },
+  {
+    name: 'Ripple (XRP)',
+    description:
+      'Ripple is both a platform and a currency that enables secure, instant, and low-cost international money transfers.',
+    slug: 'ripple',
+    seoProperties: {
+      metaTitle: 'Ripple (XRP) - Global Payment Protocol',
+      metaDescription:
+        'Discover Ripple (XRP), a blockchain technology facilitating fast and affordable cross-border transactions.',
+    },
+  },
+  {
+    name: 'Litecoin',
+    description:
+      'Litecoin is a peer-to-peer cryptocurrency created to provide fast, low-cost transactions with improved efficiency compared to Bitcoin.',
+    slug: 'litecoin',
+    seoProperties: {
+      metaTitle: 'Litecoin - Fast and Efficient Cryptocurrency',
+      metaDescription:
+        'Learn about Litecoin, a digital currency designed for speedy and cost-effective transactions.',
+    },
+  },
+  {
+    name: 'Cardano',
+    description:
+      'Cardano is a blockchain platform that aims to provide a more secure and scalable infrastructure for the development of decentralized applications and smart contracts.',
+    slug: 'cardano',
+    seoProperties: {
+      metaTitle: 'Cardano - Secure and Scalable Blockchain Platform',
+      metaDescription:
+        'Explore Cardano, a blockchain network designed for secure and scalable smart contracts and dApps.',
+    },
+  },
+];
---
+++ src/types/data.d.ts
@@ -0,0 +1,9 @@
+interface CryptoCoin {
+  name: string;
+  description: string;
+  slug: string;
+  seoProperties: {
+    metaTitle: string;
+    metaDescription: string;
+  };
+}
